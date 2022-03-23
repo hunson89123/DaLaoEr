bbe74afdc9409f8f675504f98d1b413d1232c3ce
hunson89123 <hunson89123@gmail.com>
2022-03-23 19:43:20 +0800

    新增辨別鐵支、同花順功能 待處理 同花順同時為順子問題

--- app/src/main/java/com/androidapp/hunson/dalaoer/MainActivity.java
+++ app/src/main/java/com/androidapp/hunson/dalaoer/MainActivity.java
@@ -11,13 +11,11 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.constraintlayout.widget.Guideline;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,15 +23,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_button;
@@ -42,10 +36,10 @@ public class MainActivity extends AppCompatActivity {
     ImageView[] iv_cards;
     Guideline gl_table;
     String[] cardName;
-    TextView tv_debug,tv_cardType;
-    TreeMap<Integer,String> m_cardNameTemp,m_cardName;
-    TreeMap<Integer,String> m_cardHandA,m_cardHandB,m_cardHandC,m_cardHandD;
-    List<Map.Entry<Integer,String>> l_cardNameA;
+    TextView tv_debug, tv_cardType;
+    TreeMap<Integer, String> m_cardNameTemp, m_cardName;
+    TreeMap<Integer, String> m_cardHandA, m_cardHandB, m_cardHandC, m_cardHandD;
+    List<Map.Entry<Integer, String>> l_cardNameA;
     List<Integer> l_cardNameKeys;
     List<String> l_cardSelected;
     boolean[] b_cardSelected = new boolean[13];
@@ -111,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
             iv_cards[x] = new ImageView(this);
 //            iv_cards[x].setImageResource(getResources().getIdentifier("@drawable/s" + (x + 1), null, getPackageName()));
             iv_cards[x].setImageResource(getResources().getIdentifier("@drawable/" + l_cardNameA.get(x).getValue(), null, getPackageName()));
-            Log.d("cardTable", l_cardNameA.get(x)+"");
+            Log.d("cardTable", l_cardNameA.get(x) + "");
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(i_cardW, i_cardH);
             if (x > 0) params.setMargins(i_cardL, i_selectH, 0, 0);
             else params.setMargins(0, i_selectH, 0, 0);
@@ -138,10 +132,10 @@ public class MainActivity extends AppCompatActivity {
             iv_cards[v.getId()].setY(iv_cards[v.getId()].getY() - i_selectH);
             b_cardSelected[v.getId()] = true;
         }
-        for(int x=0;x<13;x++){
-            if(b_cardSelected[x])l_cardSelected.add(l_cardNameA.get(x).getValue());
+        for (int x = 0; x < 13; x++) {
+            if (b_cardSelected[x]) l_cardSelected.add(l_cardNameA.get(x).getValue());
         }
-        tv_debug.setText("你選取的卡:"+l_cardSelected.toString().replace("[","").replace("]",""));
+        tv_debug.setText("你選取的卡:" + l_cardSelected.toString().replace("[", "").replace("]", ""));
         tv_cardType.setText(getCardType(l_cardSelected));
     }
 
@@ -149,13 +143,13 @@ public class MainActivity extends AppCompatActivity {
         //產生整副牌
         m_cardNameTemp = new TreeMap<>();
         String[] hua = new String[]{"c", "d", "h", "s"};
-        int[] num = new int[]{3,4,5,6,7,8,9,10,11,12,13,1,2};
+        int[] num = new int[]{3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 2};
         cardName = new String[52];
         for (int x = 0; x < 13; x++) {
             for (int y = 0; y < 4; y++) {
                 cardName[x * 4 + y] = hua[y] + num[x];
-                m_cardNameTemp.put(x * 4 + y,cardName[x * 4 + y]);
-                Log.d("cardRndCards", m_cardNameTemp.get(x * 4 + y)+"");
+                m_cardNameTemp.put(x * 4 + y, cardName[x * 4 + y]);
+                Log.d("cardRndCards", m_cardNameTemp.get(x * 4 + y) + "");
             }
 
         }
@@ -166,70 +160,138 @@ public class MainActivity extends AppCompatActivity {
         //分牌
         m_cardHandA = new TreeMap<>();
         for (int x = 0; x < 13; x++) {
-            m_cardHandA.put(l_cardNameKeys.get(x),m_cardNameTemp.get(l_cardNameKeys.get(x)));
-            Log.d("cardHandA", l_cardNameKeys.get(x)+"="+m_cardNameTemp.get(l_cardNameKeys.get(x))+"");
+            m_cardHandA.put(l_cardNameKeys.get(x), m_cardNameTemp.get(l_cardNameKeys.get(x)));
+            Log.d("cardHandA", l_cardNameKeys.get(x) + "=" + m_cardNameTemp.get(l_cardNameKeys.get(x)) + "");
         }
     }
 
-    public String getCardType(List<String> selected){
-        String s_cardType = "";
-        List<String> l_selectedH = new ArrayList<>();
-        List<Integer> l_selectedN = new ArrayList<>();
-        for(String s_current : selected){
-            l_selectedH.add(s_current.charAt(0)+"");
-            l_selectedN.add(Integer.parseInt(s_current.substring(1)));
+    public String getCardType(List<String> selected) {
+        StringBuilder s_cardType = new StringBuilder();
+        List<String> selectedH = new ArrayList<>();
+        List<Integer> selectedN = new ArrayList<>();
+        for (String s_current : selected) {
+            selectedH.add(s_current.charAt(0) + "");
+            selectedN.add(Integer.parseInt(s_current.substring(1)));
         }
-        if (selected.size()==1) {
-            s_cardType="單張";
-        }else if (selected. size()==2) {
-            if(l_selectedN.get(0).equals(l_selectedN.get(1))) {
-                s_cardType="對子";
-            }
-        }else if (selected.size()==5) {
-            boolean valid=true;
-            for (int i=0; i<4; i++) {
-                int current = l_selectedN.get(i);
-                int next = l_selectedN.get(i+1);
+        String[] sa_cardType = new String[]{"單張", "對子", "順子", "葫蘆", "鐵支", "同花順"};
+        boolean[] ba_cardType = new boolean[]{
+                isSingle(selected, selectedH, selectedN),
+                isPair(selected, selectedH, selectedN),
+                isStraight(selected, selectedH, selectedN),
+                isFullHouse(selected, selectedH, selectedN),
+                isFourOfaKind(selected, selectedH, selectedN),
+                isFlush(selected, selectedH, selectedN)
+        };
+        for (int x = 0; x < 6; x++)
+            if (ba_cardType[x]) s_cardType.append(sa_cardType[x]);
+
+        return s_cardType.toString();
+    }
+
+    //單張
+    public boolean isSingle(List<String> selected, List<String> selectedH, List<Integer> selectN) {
+        return selected.size() == 1;
+    }
+
+    //對子
+    public boolean isPair(List<String> selected, List<String> selectedH, List<Integer> selectedN) {
+        if (selected.size() == 2) {
+            return selectedN.get(0).equals(selectedN.get(1));
+        }
+        return false;
+    }
+
+    //順子
+    public boolean isStraight(List<String> selected, List<String> selectedH, List<Integer> selectedN) {
+        if (selected.size() == 5) {
+            boolean valid = true;
+            for (int i = 0; i < 4; i++) {
+                int current = selectedN.get(i);
+                int next = selectedN.get(i + 1);
 
                 // when the card is 'Ace' or 'Two', give it higher value according to the rule of the BigTwo game.
-                if (current == 1 || current ==0) {
-                    current+=13;
+                if (current == 2 || current == 1) {
+                    current += 13;
                 }
-                if(next == 1|| next == 0) {
-                    next+=13;
+                if (next == 2 || next == 1) {
+                    next += 13;
                 }
-                if (current!=next-1) {
-                    valid =false;
+                if (current != next - 1) {
+                    valid = false;
+                    break;
                 }
             }
-            if(valid)s_cardType="順子";
-            else{
-                valid = true;
-                int flag = l_selectedN.get(2);
-                if(l_selectedN.get(1) ==flag) {	// when the rank of center card is equal to that of the card on left.
-                    if (!l_selectedN.get(0).equals(l_selectedN.get(1))) {
-                        valid = false;
-                    }
-                    if (!l_selectedN.get(3).equals(l_selectedN.get(4))) {
-                        valid = false;
-                    }
-                }
-
-                else if (l_selectedN.get(3) == flag) {	// when the rank of center card is equal to that of the card on right.
-                    if (!l_selectedN.get(3).equals(l_selectedN.get(4))){
-                        valid = false;
-                    }
-                    if (!l_selectedN.get(0).equals(l_selectedN.get(1))) {
-                        valid = false;
-                    }
-                }
-                else
-                    valid=false;
-                if(valid)s_cardType="葫蘆";
-            }
-
+            return valid;
         }
-        return s_cardType;
+        return false;
+    }
+
+    //葫蘆
+    public boolean isFullHouse(List<String> selected, List<String> selectedH, List<Integer> selectedN) {
+        if (selected.size() == 5) {
+            boolean valid = true;
+            int flag = selectedN.get(2);
+            if (selectedN.get(1) == flag) {    // when the rank of center card is equal to that of the card on left.
+                if (!selectedN.get(0).equals(selectedN.get(1))) {
+                    valid = false;
+                }
+                if (!selectedN.get(3).equals(selectedN.get(4))) {
+                    valid = false;
+                }
+            } else if (selectedN.get(3) == flag) {    // when the rank of center card is equal to that of the card on right.
+                if (!selectedN.get(3).equals(selectedN.get(4))) {
+                    valid = false;
+                }
+                if (!selectedN.get(0).equals(selectedN.get(1))) {
+                    valid = false;
+                }
+            } else
+                valid = false;
+            return valid;
+        }
+        return false;
+    }
+
+    //鐵支
+    public boolean isFourOfaKind(List<String> selected, List<String> selectedH, List<Integer> selectedN) {
+        if (selected.size() == 5) {
+            int counter;
+            for (int i = 0; i < selected.size(); i++) {
+                counter = 0;
+                for (int j = 0; j < selected.size(); j++)
+                    if (selectedN.get(j).equals(selectedN.get(i)))
+                        counter++;
+                if (counter >= 4)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    //同花順
+    public boolean isFlush(List<String> selected, List<String> selectedH, List<Integer> selectedN) {
+        boolean valid = true;
+        if (selected.size() != 5) return false;
+        else {
+            if (isStraight(selected, selectedH, selectedN)) {
+                for (int i = 0; i < 4; i++)
+                    if (!selectedH.get(i).equals(selectedH.get(4))) {
+                        valid = false;
+                        break;
+                    }
+            } else valid = false;
+        }
+        return valid;
+    }
+
+    public void shuffle(View v){
+        randomCards();
+        ll_cards.removeAllViews();
+        tv_debug.setText("");
+        tv_cardType.setText("");
+        l_cardSelected = new ArrayList<>();
+        b_cardSelected = new boolean[13];
+        add(null);
     }
     public void getDBValue(String path) {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
